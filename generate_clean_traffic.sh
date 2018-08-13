c5e2c587b9491f396be55c06c20ca8688cc14ba8
ehc-io <eduardo@ehc.io>
2018-08-13 11:56:08 -0400

    updated description

--- generate_clean_traffic.sh
+++ generate_clean_traffic.sh
@@ -2,18 +2,18 @@
 SECONDS=0
 while true; do  
    for i in welcome.php headers.php bigtext.html httprequest.php badlinks.html ; do
-      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
-      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
-      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
-      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server5.f5demo.com/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server5.f5demo.com/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server5.f5demo.com/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server5.f5demo.com/$i
     done     
     for i in F5_building.jpg bigip4200.jpg viprion2400.jpg viprion4800.jpg ; do
-      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/images/$i
-      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/images/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server5.f5demo.com/images/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server5.f5demo.com/images/$i
     done
 sleep 1
 clear
 echo
-# echo "Baselining for L7 BDOS. Watch 'admd -s vs./Common/Auction.info -s vs./Common/Auction.sig.health' for status."
+# echo "Baselining for L7 BDOS"
 echo
 done
