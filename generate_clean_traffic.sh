51f5d785ce762beba545fb23c8557f763f36c9c7
ehc-io <eduardo@ehc.io>
2018-08-13 10:31:33 -0400

    updated test objects

--- generate_clean_traffic.sh
+++ generate_clean_traffic.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 SECONDS=0
 while true; do  
-   for i in welcome.php headers.php bigtext.html httprequest.php badlinks.html f5logo.gif; do
+   for i in welcome.php headers.php bigtext.html httprequest.php badlinks.html ; do
       curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
       curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
       curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
       curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
     done     
-    for i in F5_building.jpg bigip4200.jpg ; do
+    for i in F5_building.jpg bigip4200.jpg viprion2400.jpg viprion4800.jpg ; do
       curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/images/$i
       curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/images/$i
     done
