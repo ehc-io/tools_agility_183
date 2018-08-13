5800173d06799169c37741fd124de634a577191b
ehc-io <eduardo@ehc.io>
2018-08-13 13:44:21 -0400

    added https protocol on test_server1 script

--- server2_http_test.sh
+++ server2_http_test.sh
@@ -2,7 +2,7 @@
 SECONDS=0
 while true; do  
    for i in welcome.php bigtext.html httprequest.php ; do
-      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server1.f5demo.com/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://server1.f5demo.com/$i
     done     
 sleep 1
 clear
