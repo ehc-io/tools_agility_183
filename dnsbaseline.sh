b07adc8c2576bfc7d4b646b81089e006a4e9bc18
ehc-io <eduardo@ehc.io>
2018-08-13 07:40:55 -0400

    interval update

--- dnsbaseline.sh
+++ dnsbaseline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 	echo "Starting DNS baseline with 50 A Queries/S"
-	echo "dnsperf -s 10.1.20.14 -d dnsbaseline.txt -Q 50 -S 5 -c 100 -l 30"
+	echo "dnsperf -s 10.1.20.14 -d dnsbaseline.txt -Q 50 -S 5 -c 100 -l 120"
 	docker run -v ~/tools_agility_183:/root/tools_agility_183 -it mrlesmithjr/dnsperf \
-    	dnsperf -s 10.1.20.14 -d /root/tools_agility_183/dnsbaseline.txt -Q 50 -S 5 -c 100 -l 30
+    	dnsperf -s 10.1.20.14 -d /root/tools_agility_183/dnsbaseline.txt -Q 50 -S 5 -c 100 -l 120
