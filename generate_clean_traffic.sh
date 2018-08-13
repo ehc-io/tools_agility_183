6fbddf912be38d8db819c6fbf59b178f57dd0c9f
ehc-io <eduardo@ehc.io>
2018-08-13 10:24:26 -0400

    updated test objects

--- generate_clean_traffic.sh
+++ generate_clean_traffic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SECONDS=0
 while true; do  
-   for i in welcome.php images/F5_building.jpg headers.php bigtext.html images/bigip4200.jpg httprequest.php; do
+   for i in welcome.php images/F5_building.jpg headers.php bigtext.html images/bigip4200.jpg httprequest.php viprion2400.jpg badlinks.html f5logo.gif; do
       curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
       curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
       curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
