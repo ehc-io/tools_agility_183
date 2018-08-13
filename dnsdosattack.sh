7eb5468c3fde8297372301d2e3a10a1e63708184
ehc-io <eduardo@ehc.io>
2018-08-13 08:05:46 -0400

    added docker container

--- dnsdosattack.sh
+++ dnsdosattack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Starting DNS baseline with 80 A Queries/s"
 echo "dnsperf -s 10.1.20.14 -d dnsbaseline.txt -Q 80 -S 5 -c 100 -l 360"
-dnsperf -s 10.1.20.14 -d dnsbaseline.txt -Q 80 -S 5 -c 100 -l 360
+docker run -v ~/tools_agility_183:/root/tools_agility_183 -it mrlesmithjr/dnsperf \
+    	dnsperf -s 10.1.20.14 -d /root/tools_agility_183/dnsbaseline.txt -Q 80 -S 5 -c 100 -l 360
