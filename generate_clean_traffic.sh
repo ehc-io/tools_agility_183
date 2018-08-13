3ab38465ddfe893e8ee1a2810709d04791f48d9b
ehc-io <eduardo@ehc.io>
2018-08-13 10:43:07 -0400

    updated echo

--- generate_clean_traffic.sh
+++ generate_clean_traffic.sh
@@ -2,14 +2,14 @@
 SECONDS=0
 while true; do  
    for i in welcome.php headers.php bigtext.html httprequest.php badlinks.html ; do
-      curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
-      curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
-      curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
-      curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/$i
     done     
     for i in F5_building.jpg bigip4200.jpg viprion2400.jpg viprion4800.jpg ; do
-      curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/images/$i
-      curl -s -m 1 -o /dev/null -w "$i.html\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/images/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/images/$i
+      curl -s -m 1 -o /dev/null -w "$i\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" server2.f5demo.com/images/$i
     done
 sleep 1
 clear
